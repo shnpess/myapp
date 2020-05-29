12cca9fcd30c56426440ccc3f0e0aef568f55543
shnpess <ann2lax@gmail.com>
2020-05-29 22:47:54 +0900

    投稿一覧ページの修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,11 +8,7 @@ class Post < ApplicationRecord
   has_many :groups
   has_many :category_posts
   has_many :categories, through: :category_posts
-  def self.search(search)
-    return Post.all unless search
-    Post.joins(:user).where(['gender LIKE ? OR content LIKE ? OR kind Like ? OR name LIKE ? OR age LIKE ? OR vaccination LIKE ? OR addres LIKE ?', "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%"])
-   
-  end
+ 
 
 
 end
