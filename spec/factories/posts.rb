ca3a5319b884bb3c0ee45343caf5c10ca55bb01d
shnpess <ann2lax@gmail.com>
2020-06-07 21:06:39 +0900

    Revert "test: テスコードを追加"
    This reverts commit 33c4e7d850f8184353b6e8564e28bd863fbd18c0.

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
 
-  factory :post do
+  factory :user do
     name              {"ころ"}
     kind              {"アメリカンショートヘアー"}
     gender            {"オス"}
@@ -10,14 +10,6 @@ FactoryBot.define do
     image             {"猫1.jpg"}
     user_id           {1}
     content           {"テストテスト"}
-  end
-
-  factory :user do
-    nickname          {"mori"}
-    addres            {"福岡県"}
-    id                {"1"}
-    email             {"aaa@aaa"}
-    password          {"aaaaaa"}
   end
 
 end
