1558037bf1c4528491faaf31fd75c57349dfd2aa
shnpess <ann2lax@gmail.com>
2020-07-07 01:01:33 +0900

    [add]groupモデルのアソシエーションのテストを追加

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -17,4 +17,45 @@ describe Group do
       expect(group.errors[:name]).to include("を入力してください")
     end
   end
+
+  describe 'Association' do
+    before do
+      FactoryBot.build(:user)
+      FactoryBot.build(:comment)
+      FactoryBot.build(:like)
+      @post = FactoryBot.build(:post)
+    end
+
+    let(:association) do
+      described_class.reflect_on_association(target)
+    end
+    context 'Userテーブルとのアソシエーション' do
+      let(:target) { :users }
+      it "Userとの関連付けはbelongs_toであること" do
+        expect(association.macro).to eq :has_many
+      end
+    end
+
+    context 'Postテーブルとのアソシエーション' do
+      let(:target) { :post }
+      it "Postとの関連付けはhas_manyであること" do
+        expect(association.macro).to eq :belongs_to
+      end
+    end
+
+    context 'GroupsUserテーブルとのアソシエーション' do
+      let(:target) { :group_users }
+      it "GroupsUserとの関連付けはhas_manyであること" do
+        expect(association.macro).to eq :has_many
+      end
+    end
+
+    context ' Messageテーブルとのアソシエーション' do
+      let(:target) { :messages }
+      it "Messageとの関連付けはbelongs_toであること" do
+        expect(association.macro).to eq :has_many
+      end
+    end
+
+  end
 end
